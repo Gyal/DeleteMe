f18f698a48a0cce03ac36efd993cff55f3fbb426
melinaM <melina.merabli@live.fr>
2015-03-14 00:08:58 +0100

    gg

---
+++ src/main/java/fr/iut/montreuil/lpcsid/DeleteMeApplication.java
@@ -0,0 +1,26 @@
+package fr.iut.montreuil.lpcsid;/*
+ * Created by Mélina on 07/03/2015.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class DeleteMeApplication extends SpringBootServletInitializer {
+
+    public static void main(final String[] args) throws Exception {
+        SpringApplication.run(DeleteMeApplication.class, args);
+    }
+
+    // SERVLETinitializer
+    @Override
+    protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application){
+        return application.sources(DeleteMeApplication.class);
+    }
+}
